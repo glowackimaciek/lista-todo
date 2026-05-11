c12fae3c416a9916c25f65b012c5f67d14780193
Maciej <macglowacki@gmail.com>
2026-05-11 10:52:24 +0200

    Dodanie klasy lista zadan

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 class Zadanie:
-    def __init__(self, nazwa, status, numer):
+    def __init__(self, nazwa, status, numer=""):
         self.nazwa = nazwa
         self.status = status
         self.numer = numer
@@ -8,3 +8,25 @@ class Zadanie:
         return (
             f"Nr zadania: {self.numer} | Zadanie: {self.nazwa} | Status: {self.status}"
         )
+
+
+class Lista_Zadań:
+    def __init__(self, nazwa):
+        self.nazwa = nazwa
+        self.lista_zadan = []
+
+    def dodaj_zadanie(self, nazwa, status):
+        numer = len(self.lista_zadan) + 1
+        nowe = Zadanie(nazwa, status, numer)
+        self.lista_zadan.append(nowe)
+
+    def pokaz_zadania(self):
+        for zadanie in self.lista_zadan:
+            print(zadanie.zadanie_info)
+
+    def usun_zadanie(self, numer):
+        nowa_lista = []
+        for zadanie in self.lista_zadan:
+            if zadanie.numer != numer:
+                nowa_lista.append(zadanie)
+        self.lista_zadan = nowa_lista
